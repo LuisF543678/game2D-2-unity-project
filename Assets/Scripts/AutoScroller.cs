c738890baa3762fc76d2ae82de46237bcca9acd8
Luisf543678 <luisf543678@gmail.com>
2021-10-08 19:35:15 -0500

    moving stage

---
+++ Assets/Scripts/AutoScroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoScroller : MonoBehaviour
+{
+    public float scrollerSpeed = 0.9f;
+    private MeshRenderer renderer;
+    [SerializeField] private MeshRenderer mesh;
+    
+    void Update()
+    {
+        Vector2 offset = new Vector2(0, Time.time * scrollerSpeed);
+        mesh.material.mainTextureOffset = offset;
+    }
+}
